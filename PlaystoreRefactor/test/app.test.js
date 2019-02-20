ea59b85e057104eda6440b0388d8f8824c25c776
AReach <DerAReich@gmail.com>
2019-02-20 14:57:23 -0600

    finished genre test

--- PlaystoreRefactor/test/app.test.js
+++ PlaystoreRefactor/test/app.test.js
@@ -20,42 +20,28 @@ describe('GET /apps', () => {
     it('should require genre to be one of Action, Puzzle, Strategy, Casual, Arcade or Card', () => {
       return request(app)
         .get('/apps')
-        .query({genre:'ERROR'})
+        .query({ genre: 'ERROR' })
         .expect(400, 'Genre must be one of Action, Puzzle, Strategy, Casual, Arcade or Card');
     });
 
     it('should only return apps of the specified genre', () => {
       const queries = ['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'];
-      // return queries.map((query) => {
-      //   return request(app)
-      //     .get('/apps')
-      //     .query({query})
-      //     .expect(200)
-      //     .expect('content-type', /json/)
-      //     .then(resp => {
-      //       let i = 0;
-      //       let genre = true;
-      //       while(genre && i < resp.body.length) {
-      //         genre = genre && resp.body[i].Genres.toLowerCase().includes(query);
-      //         i++;
-      //       }
-      //       expect(genre).to.be.true;
-      //     });
-      // });
-      return request(app)
-        .get('/apps')
-        .query({query: 'action'})
-        .expect(200)
-        .expect('content-type', /json/)
-        .then(resp => {
-          let i = 0;
-          let genre = true;
-          while(genre && i < resp.body.length - 1) {
-            genre = genre && resp.body[i].Genres.toLowerCase().indexOf('action');
-            i++;
-          }
-          expect(genre).to.be.true;
-        });
+      return queries.map((query) => {
+        return request(app)
+          .get('/apps')
+          .query({ genre: query })
+          .expect(200)
+          .expect('content-type', /json/)
+          .then(resp => {
+            let i = 0;
+            let genre = true;
+            while (genre && i < resp.body.length - 1) {
+              genre = genre && resp.body[i].Genres.toLowerCase().includes(query);
+              i++;
+            }
+            expect(genre).to.be.true;
+          });
+      });
     });
   });
 
@@ -63,22 +49,22 @@ describe('GET /apps', () => {
     it('should require sort to be rating or app', () => {
       return request(app)
         .get('/apps')
-        .query({sort:'ERROR'})
+        .query({ sort: 'ERROR' })
         .expect(400, 'Sort must be by rating or app name');
     });
 
     it('should return a sorted array by rating', () => {
       return request(app)
         .get('/apps')
-        .query({sort: 'rating'})
+        .query({ sort: 'rating' })
         .expect(200)
         .expect('content-type', /json/)
         .then(resp => {
           expect(resp.body).to.be.an('array');
           let i = 0;
           let sorted = true;
-          while(sorted && i < resp.body.length - 1) {
-            sorted = sorted && resp.body[i].Rating <= resp.body[i+1].Rating;
+          while (sorted && i < resp.body.length - 1) {
+            sorted = sorted && resp.body[i].Rating <= resp.body[i + 1].Rating;
             i++;
           }
           expect(sorted).to.be.true;
@@ -88,15 +74,15 @@ describe('GET /apps', () => {
     it('should return a sorted array by app name', () => {
       return request(app)
         .get('/apps')
-        .query({sort: 'app'})
+        .query({ sort: 'app' })
         .expect(200)
         .expect('content-type', /json/)
         .then(resp => {
           expect(resp.body).to.be.an('array');
           let i = 0;
           let sorted = true;
-          while(sorted && i < resp.body.length - 1) {
-            sorted = sorted && resp.body[i].App.toLowerCase() < resp.body[i+1].App.toLowerCase();
+          while (sorted && i < resp.body.length - 1) {
+            sorted = sorted && resp.body[i].App.toLowerCase() < resp.body[i + 1].App.toLowerCase();
             i++;
           }
           expect(sorted).to.be.true;
